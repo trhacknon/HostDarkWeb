3149114aebe0a7eb52c9d5cc59f9852c6acf78ee
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-07 12:17:34 +0530

    Update Logo.sh

--- OnlineHacking/Logo.sh
+++ OnlineHacking/Logo.sh
@@ -9,12 +9,12 @@ echo ""
 banner() {
 echo -e ""
 echo -e "\e[95m
-              ██   ██  ██████  ███████ ████████                             
-              ██   ██ ██    ██ ██         ██                                
-              ███████ ██    ██ ███████    ██                                
-              ██   ██ ██    ██      ██    ██                                
-              ██   ██  ██████  ███████    ██      \e[0m\n                          
-                                                              
+               ██   ██  ██████  ███████ ████████                             
+               ██   ██ ██    ██ ██         ██                                
+               ███████ ██    ██ ███████    ██                                
+               ██   ██ ██    ██      ██    ██                                
+               ██   ██  ██████  ███████    ██                         
+                                         \e[0m\n                            
 \e[33m                                                              
 ██████   █████  ██████  ██   ██     ██     ██ ███████ ██████  
 ██   ██ ██   ██ ██   ██ ██  ██      ██     ██ ██      ██   ██ 
@@ -25,11 +25,11 @@ echo -e "\e[95m \e[0m\n"
   echo""    
   echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;95m\e[0m\e[1;32m Host Daek Website \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m\e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m\e[1;32m\e[0m\e[1;33m]\e[0m'
    echo ""
 sleep 1
 echo ""
-echo -e "              \033[1;91m  Author   \033[1;90m:  \033[1;95mOnline HacKing (SUMAN)"
+echo -e "              \033[1;91m  Author   \033[1;90m:  \033[1;95mOnline HacKing \033[1;97m(SUMAN)"
 echo -e "              \033[1;93m Telegram   \033[1;90m: \033[1;96m@OnlineHacking"
 echo -e "              \033[1;92m  Website   \033[1;90m: \033[1;94mwww.onlinehacking.in"
 echo -e "              \033[1;97m  Website   \033[1;90m: \033[1;93mwww.onlinehacking.xyz"
