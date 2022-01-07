c1f9fa03cc674c3c6c425c24b5016ef01dc12c47
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-07 11:46:57 +0530

    Create Logo.sh

---
+++ OnlineHacking/Logo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+banner() {
+echo -e ""
+echo -e "\e[33m
+              ██   ██  ██████  ███████ ████████                             
+              ██   ██ ██    ██ ██         ██                                
+              ███████ ██    ██ ███████    ██                                
+              ██   ██ ██    ██      ██    ██                                
+              ██   ██  ██████  ███████    ██                                
+                                                              
+                                                              
+██████   █████  ██████  ██   ██     ██     ██ ███████ ██████  
+██   ██ ██   ██ ██   ██ ██  ██      ██     ██ ██      ██   ██ 
+██   ██ ███████ ██████  █████       ██  █  ██ █████   ██████  
+██   ██ ██   ██ ██   ██ ██  ██      ██ ███ ██ ██      ██   ██ 
+██████  ██   ██ ██   ██ ██   ██      ███ ███  ███████ ██████  
+                                                                                                                                                                                      
+                                                              \e[0m\n"
+echo -e "\e[95m 
+figlet -f slant Online Hacking
+
+\e[95;1m \e[0m\n"
+  echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Host Hidden Daek Web Tool \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo ""
+sleep 1
+echo ""
+echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95mAnubhav Kashyap"
+echo -e "              \033[1;93m  Group    \033[1;90m: \033[1;96mDeepweb Shadows"
+echo -e "              \033[1;92m Instagram \033[1;90m: \033[1;94m@anubhavanonymous"
+echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/anubhavanonymous"
+echo ""
+echo -e ""
+sleep 1                                               
+      }
+       banner
