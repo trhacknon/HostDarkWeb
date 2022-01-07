47da85a5495348c9f6379973a15116da156ce8b5
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-01-07 12:03:54 +0530

    Update Logo.sh

--- OnlineHacking/Logo.sh
+++ OnlineHacking/Logo.sh
@@ -4,7 +4,7 @@ white='\e[1;37m'
 green='\e[0;32m'
 blue='\e[1;34m'
 red='\e[1;31m'
-yellow='\e[1;33m' 
+yellow='\e[1;33m'
 echo ""
 banner() {
 echo -e ""
@@ -22,19 +22,17 @@ echo -e "\e[33m
 ██   ██ ██   ██ ██   ██ ██  ██      ██ ███ ██ ██      ██   ██ 
 ██████  ██   ██ ██   ██ ██   ██      ███ ███  ███████ ██████  \e[0m\n"
 echo -e "\e[95m \e[0m\n"
-figlet -f slant Online Hacking
-
   echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Host Hidden Daek Web Tool \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
+  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Host Daek Website \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m\e[1;32m\e[0m\e[1;33m]\e[0m'
    echo ""
 sleep 1
 echo ""
-echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95mAnubhav Kashyap"
-echo -e "              \033[1;93m  Group    \033[1;90m: \033[1;96mDeepweb Shadows"
-echo -e "              \033[1;92m Instagram \033[1;90m: \033[1;94m@anubhavanonymous"
-echo -e "              \033[1;97m  Github   \033[1;90m: \033[1;93mgithub.com/anubhavanonymous"
+echo -e "              \033[1;91m  Author   \033[1;90m: \033[1;95mSuman Mondal"
+echo -e "              \033[1;93m Telegram   \033[1;90m: \033[1;96m@OnlineHacking"
+echo -e "              \033[1;92m  Website   \033[1;90m: \033[1;94mwww.onlinehacking.in"
+echo -e "              \033[1;97m  Website   \033[1;90m: \033[1;93www.onlinehacking.xyz"
 echo ""
 echo -e ""
 sleep 1                                               
